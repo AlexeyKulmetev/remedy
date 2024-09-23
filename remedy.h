7222ddbe9d138efccea3347633092623d542e8c5
Aleksey_home_linux <pervoclassnic@gmail.com>
2024-09-23 18:26:51 +0300

    Created blueprints of classes

--- remedy.h
+++ remedy.h
@@ -3,13 +3,46 @@
 
 
 #include <string>
+#include <vector>
 
 
 class Remedy {
 protected:
-    std::string Name;
-    int Duration;
+    std::string _Name;
+    int _treatmentDays;
+    int _frequencyPerDay;
+
+public:
+    virtual void read();
+    virtual void write() const;
 };
 
+
+class MedicalProcedure : public Remedy {
+protected:
+    std::string _executionConditions;
+    int _minutesRequired;
+
+public:
+    virtual void read() override;
+    virtual void write() const override;
+};
+
+
+class Medicine : public Remedy {
+protected:
+    std::string _intakeMethod;
+    float _doseUnits;
+
+public:
+    virtual void read() override;
+    virtual void write() const override;
+};
+
+
+class TreatmentCourse {
+private:
+    std::vector<Remedy*> _remedyList;
+};
 
 #endif
